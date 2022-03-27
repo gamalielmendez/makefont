4e845aaa37e46f70ce6e0a7e81f7b9e489a7a67c
Gamaliel Méndez <gamalielmh921230@hotmail.com>
2022-03-26 20:04:53 -0600

    *se agregaron comando de version y ayuda (-v,-h) *sube de version

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,38 @@
 #!/usr/bin/env node
 import MakeFont from "./makefont"
 
+let ValidFlags=['--v','-v',
+                '-h','--h']
+
     if(process.argv.length===2){
         console.log("Usage:  makefont_njs fontfile [encoding] [embed] [subset]\n")
     }else{
         
-        let fontfile = process.argv[2];
-        let enc="cp1252"
-        let embed=true
-        let subset=true
+        if(!ValidFlags.includes(process.argv[2])){
 
-        if(process.argv.length>=4){
-            enc=process.argv[3];
+            let fontfile = process.argv[2];
+            let enc="cp1252"
+            let embed=true
+            let subset=true
+    
+            if(process.argv.length>=4){
+                enc=process.argv[3];
+            }
+    
+            if(process.argv.length>=5){
+                embed=(process.argv[4]=='true' || process.argv[4]=='1');
+            }
+    
+            if(process.argv.length>=6){
+                subset=(process.argv[5]=='true' || process.argv[5]=='1');
+            }
+    
+            MakeFont(fontfile,enc,embed,subset)
+        }else if(['--v','-v'].includes(process.argv[2])){
+            console.log('makefont_njs ',process.version)
+        }else if(['-h','--h'].includes(process.argv[2])){
+            console.log("Usage:  makefont_njs fontfile [encoding] [embed] [subset]\n")
         }
 
-        if(process.argv.length>=5){
-            embed=(process.argv[4]=='true' || process.argv[4]=='1');
-        }
-
-        if(process.argv.length>=6){
-            subset=(process.argv[5]=='true' || process.argv[5]=='1');
-        }
-
-        MakeFont(fontfile,enc,embed,subset)
     }   
      
